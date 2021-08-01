8cf728fe3222a53ca43a468beb256fea6e2ae017
FCO <franne_@hotmail.com>
2021-08-01 20:17:08 +0200

    update reminder even after refresh fixed

--- fco-angular/src/app/services/task.service.ts
+++ fco-angular/src/app/services/task.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Task } from '../Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
  
 @Injectable({
   providedIn: 'root'
@@ -21,6 +27,11 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 
 }
